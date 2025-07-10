1caf9a8d9a636c665cc50eae769f0ca5b7317f70
kamstand <kamrul.hassan@stonybrook.edu>
2025-07-10 02:10:39 -0400

    created vite project and spring-boot project

---
+++ server/shokify/src/main/java/com/ecomm/shokify/ShokifyApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.shokify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShokifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShokifyApplication.class, args);
+	}
+
+}
---
+++ server/shokify/src/test/java/com/ecomm/shokify/ShokifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.shokify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShokifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
